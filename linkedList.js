5eda8dedad1c0e0629f60bca0833bca84d772630
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-06-14 11:18:34 +0300

    Add prepend method in linked list class

--- linkedList.js
+++ linkedList.js
@@ -25,6 +25,22 @@ class LinkedList {
     this.list += '->'
     console.log(this.list)
   }
+
+  prepend (value) {
+    const newNode =  new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.list += newNode.value;
+      this.list += '->'
+      console.log(this.list)
+      return
+    }
+    let prevHead = this.head;
+    this.head = newNode;
+    this.head.nextNode = prevHead;
+    this.list = `${newNode.value}->` + this.list
+    console.log(this.list)
+  }
 }
 
 export default LinkedList
